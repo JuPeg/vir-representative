5d96db71613cb3eb048e9d427dc98b7480c26672
JuPeg <juliana_pegoraro@hotmail.com>
2016-05-19 15:21:04 +0200

    parallelize jobs

--- vir_represen.sh
+++ vir_represen.sh
@@ -26,14 +26,20 @@ date "+%Y-%m-%d %H:%M:%S"
 
 #Retrieve RefSeq sequences
 printf "Retrieve RefSeq sequences\n"
-$retrieve_fasta -i "$queryString AND refseq[Filter]" -d $dbname -o $outfilename"_"RS -l $outfilename"_"RS.log
-date "+%Y-%m-%d %H:%M:%S"
+retrieve_fasta -i "$queryString AND refseq[Filter]" -d $dbname -o $outfilename"_"RS -l $outfilename"_"RS.log &
+pids[1]=$!
 
 #Retrieve not RefSeq sequences
 printf "Retrieve not-RefSeq sequences\n"
-$retrieve_fasta -i "$queryString NOT refseq[Filter]" -d $dbname -o $outfilename"_"NOTRS -l $outfilename"_"NOTRS.log
-date "+%Y-%m-%d %H:%M:%S"
+retrieve_fasta -i "$queryString NOT refseq[Filter]" -d $dbname -o $outfilename"_"NOTRS -l $outfilename"_"NOTRS.lo &
+pids[2]=$!
 
+for pid in ${pids[*]};
+    do wait $pid;
+done;
+
+printf "Sequences retrieved from NCBI at: "
+date "+%Y-%m-%d %H:%M:%S"
 #Merge, sort, filter by length
 printf "Merge, sort and filter\n"
 cat $outfilename"_"RS $outfilename"_"NOTRS | awk '/^>/ {printf("\t%i\n%s\t",len, $0);len=0;next; } { printf("%s",$0); len+=length($0) } END {printf("\t%s",len);}' | sort -r -s -k3 -n | awk -v T=$threshold '{if($3>T){ printf("%s\n%s\n",$1,$2) > "'''$longfile'''"; }else { printf("%s\n%s\n",$1,$2) > "'''$shortfile'''"} }'
@@ -41,16 +47,24 @@ date "+%Y-%m-%d %H:%M:%S"
 
 #PSI-CD-HIT
 printf "Psi-cd-hit\n" 
-$psicdhit -i $longfile -o $longout -c $id -prog megablast
-date "+%Y-%m-%d %H:%M:%S"
+psicdhit -i $longfile -o $longout -c $id -prog megablast &
+pids[1]=$!
 
 #USEARCH
-printf "Usearch: "
-$usearch -cluster_smallmem $shortfile -id $id -sortedby length -uc $uc -centroids $centroids
-date "+%Y-%m-%d %H:%M:%S"
+printf "Usearch\n"
+usearch -cluster_smallmem $shortfile -id $id -sortedby length -uc $uc -centroids $centroids &
+pids[2]=$!
 
+for pid in ${pids[*]};
+    do wait $pid;
+done;
+
+printf "Finished clustering at: "
+date "+%Y-%m-%d %H:%M:%S"
 #Representative sequences:
-printf "Representative sequences in file %s\n" $representatives 
+printf "Representative sequences in file %s/%s\n" $(pwd) $representatives 
 cat $longout $centroids > $representatives
 printf "\nProcess finished at "
 date "+%Y-%m-%d %H:%M:%S"
+
+cd ..
